cd274ea76988090d17143076a2d9071ec71fabf3
Shubhang Narain <snvishere@gmail.com>
2023-09-03 18:15:09 +0530

    added social links

--- components/Connect.js
+++ components/Connect.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import Link from 'next/link'
 
 import { FiGithub } from 'react-icons/fi'
 import { BsInstagram } from 'react-icons/bs'
@@ -10,10 +11,23 @@ const Connect = () => {
     <div className='p-10 m-5 rounded-2xl grid grid-flow-col lg:grid-flow-row sm:justify-between lg:justify-center items-center
          bg-gray-900 bg-clip-padding backdrop-filter backdrop-blur-sm
         bg-opacity-20 hover:scale-[105%] ease-in-out duration-300 text-white'>
-        <FiGithub className='hover:scale-[125%] ease-in-out duration-300' size={40}/>
-        <BsInstagram className='hover:scale-[125%] ease-in-out duration-300' size={40}/>
-        <FaDev className='hover:scale-[125%] ease-in-out duration-300' size={40}/>
-         <GrFacebook className='hover:scale-[125%] ease-in-out duration-300' size={40}/>
+
+        <Link href="https://github.com/shubhangNarain">
+          <FiGithub className='hover:scale-[125%] ease-in-out duration-300' size={40}/>
+        </Link>
+
+        <Link href="https://www.instagram.com/_.snv.__/">
+          <BsInstagram className='hover:scale-[125%] ease-in-out duration-300' size={40}/>
+        </Link>
+        
+        <Link href="https://dev.to/shubhangnarain">
+          <FaDev className='hover:scale-[125%] ease-in-out duration-300' size={40}/>
+        </Link>
+
+        <Link href="https://www.facebook.com/profile.php?id=100008802099909">
+          <GrFacebook className='hover:scale-[125%] ease-in-out duration-300' size={40}/>
+        </Link>       
+        
     </div>
   )
 }
--- components/Contact.js
+++ components/Contact.js
@@ -2,7 +2,36 @@ import React from 'react'
 
 const Contact = () => {
   return (
-    <div>Contact</div>
+    <div className='p-10 flex items-start'>
+        <form className='grid grid-rows-3'>
+            <div className='p-5 text-7xl font-bold '>
+                Get In Touch
+            </div>
+
+            <div className='flex flex-col'>
+                <div className='grid grid-cols-2'>
+                    <span className='py-2 mx-2 text-2xl italic font-light'>First Name</span>
+                    <span className='py-2 mx-2 text-2xl italic font-light'>Last Name</span>
+                </div>
+                <div className='grid grid-cols-2'>
+                    <input className='mx-2 p-2 rounded-xl' type='text' placeholder='First Name' />
+                    <input className='mx-2 p-2 rounded-xl' type='text' placeholder='Last Name' />
+                </div>
+
+                <div className='grid grid-rows-2'>
+                    <span className='py-2 mx-2 text-2xl italic font-light'>Email</span>
+                    <input className='mx-2 p-2 rounded-xl' type='text' placeholder='Email address' />
+                </div>
+            </div>
+
+            <div className='grid grid-rows-2'>
+                    <span className='py-5 mx-2 text-2xl italic font-light'>Message</span>
+                    <input className='p-0 m-2 p-2 rounded-xl' type='text' placeholder='How we doin today?!' />
+            </div>
+
+
+        </form>
+    </div>
   )
 }
 
